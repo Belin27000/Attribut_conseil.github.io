8d879973493bc4846bf6c0d6dd1ac6033ce2c4c0
Belin27000 <y.lecerf@protonmail.com>
2023-09-08 09:54:50 +0200

    feat:about pages building

--- src/Pages/Apropos/About.js
+++ src/Pages/Apropos/About.js
@@ -4,45 +4,88 @@ import { Qualiopi, DataDoc } from '@/Pages/index.js'
 
 const About = () => {
     return (
-        <div className='About'>
+        <section className='About'>
 
             {/* QUI SOMMES NOUS */}
-            <div>
-                <h2>ATTRIBUT CONSEILS, EXPERT DE LA RELATION HOMME/ TRAVAIL</h2>
-                <div className='aboutPresentation'>
+            <h2>ATTRIBUT CONSEILS, EXPERT DE LA RELATION HOMME/ TRAVAIL</h2>
+
+            <div className='AboutPres'>
+                <p>
                     Nous intervenons sur l’ensemble des sujets concernant le travail :
+                </p>
+                <p>
                     La gestion des carrières professionnelles bilans de compétences, de mobilité professionnelle, la psychologie du travail, le développement des compétences par la formation professionnelle, la gestion de la formation du point de vue de l’ingénierie et de la qualité,  les risques psychosociaux  auprès de tous types d’entreprises et en en direction des salariés et des particuliers de tous secteurs et niveaux.
-                </div>
+                </p>
             </div>
-            <div>
-                Nous sommes passionnés par le sujet du travail et de l'apprentissage. Nous croyons fermement que ce domaine, essentiel dans nos vies, devrait être une source d'épanouissement et d'élévation plutôt qu'une source de souffrance. Notre engagement est de cultiver une approche attentive afin de favoriser l'harmonie dans nos vies. Pour y parvenir, cela nécessite un effort personnel constant pour se développer et s'améliorer, ainsi qu'un travail sur notre relation aux autres et notre communication. En combinant ces éléments, nous pouvons créer des environnements de travail où le bonheur et l'épanouissement sont possibles pour tous. Ensemble, osons le bonheur en entreprise en faisant de cet idéal une réalité.
+            <div className='AboutPres'>
+                <p>
+                    Nous sommes passionnés par le sujet du travail et de l'apprentissage.
+                </p>
+                <p>
+                    Nous croyons fermement que ce domaine, essentiel dans nos vies, devrait être une source d'épanouissement et d'élévation plutôt qu'une source de souffrance. Notre engagement est de cultiver une approche attentive afin de favoriser l'harmonie dans nos vies. Pour y parvenir, cela nécessite un effort personnel constant pour se développer et s'améliorer, ainsi qu'un travail sur notre relation aux autres et notre communication. En combinant ces éléments, nous pouvons créer des environnements de travail où le bonheur et l'épanouissement sont possibles pour tous. Ensemble, osons le bonheur en entreprise en faisant de cet idéal une réalité.
+                </p>
             </div>
-            <div>
-                ATTRIBUT CONSEILS à plus de 16 ans maintenant, et reçoit plus de 300 salariés par an de tous secteurs, tous niveaux sur des problématiques très diverses liées au travail. Notre équipe est composée de psychologues du travail, de consultants experts bilan, de coachs et thérapeutes bref diplômés, d'ingénieurs de formation, de chef de projets blended learning, d’anciens RRH et dirigeants d’entreprise. Attribut conseils et son équipe attachent une grande importance à la formation continue et nous sommes en permanence dans un objectif de qualification par la formation et par la supervision.
+            <div className='AboutPres'>
+                <p>
+                    ATTRIBUT CONSEILS à plus de 16 ans maintenant, et reçoit plus de 300 salariés par an de tous secteurs, tous niveaux sur des problématiques très diverses liées au travail.
+                </p>
+                <p>
+                    Notre équipe est composée de psychologues du travail, de consultants experts bilan, de coachs et thérapeutes bref diplômés, d'ingénieurs de formation, de chef de projets blended learning, d’anciens RRH et dirigeants d’entreprise. Attribut conseils et son équipe attachent une grande importance à la formation continue et nous sommes en permanence dans un objectif de qualification par la formation et par la supervision.
+                </p>
             </div>
-            <div>
-                Notre expertise englobe un large éventail de domaines lies au travail, ce qui nous permet d'intervenir de manière exhaustive sur tous les aspects essentiels. Nous comprenons l'importance de la gestion des carrières professionnelles, car elle permet aux individus de tracer leur chemin vers le succès et l'épanouissement dans le monde du travail. En nous appuyant sur des méthodes éprouvées et une connaissance approfondie des dynamiques professionnelles, nous aidons nos clients à élaborer des plans de carrière solides et à prendre des décisions éclairées pour progresser dans leur domaine.
+            <div className='AboutPres-center'>
+                <h3 >
+                    Notre expertise englobe un large éventail de domaines lies au travail, ce qui nous permet d'intervenir de manière exhaustive sur tous les aspects essentiels.
+                </h3>
+                <p>
+
+                    Nous comprenons l'importance de la gestion des carrières professionnelles, car elle permet aux individus de tracer leur chemin vers le succès et l'épanouissement dans le monde du travail. En nous appuyant sur des méthodes éprouvées et une connaissance approfondie des dynamiques professionnelles, nous aidons nos clients à élaborer des plans de carrière solides et à prendre des décisions éclairées pour progresser dans leur domaine.
+                </p>
+
             </div>
-            <div>
-                La psychologie du travail est un autre domaine central de notre expertise. Nous comprenons les enjeux psychologiques qui accompagnent le travail, tels que le stress, la motivation, la satisfaction professionnelle et les relations interpersonnelles. En utilisant des approches psychologiques avancées, nous offrons un soutien adapté aux individus et aux organisations pour améliorer leur bien-être mental, leur productivité et leur climat de travail.
+            <div className='AboutPres'>
+                <p>
+                    La psychologie du travail est un autre domaine central de notre expertise.
+                </p>
+                <p>
+                    Nous comprenons les enjeux psychologiques qui accompagnent le travail, tels que le stress, la motivation, la satisfaction professionnelle et les relations interpersonnelles. En utilisant des approches psychologiques avancées, nous offrons un soutien adapté aux individus et aux organisations pour améliorer leur bien-être mental, leur productivité et leur climat de travail.
+                </p>
             </div>
-            <div>
-                Le développement des compétences par le biais de la formation professionnelle est une autre facette cruciale de notre intervention. Nous croyons fermement que l'apprentissage continu est essentiel pour s'adapter aux évolutions du marché du travail et aux nouvelles exigences professionnelles. La formation est l’un des leviers de la motivation. Nous concevons des programmes de formation personnalisés qui ciblent les lacunes spécifiques des individus et des équipes, en utilisant des méthodologies pédagogiques innovantes pour maximiser l'acquisition de connaissances et le développement des compétences.
+            <div className='AboutPres'>
+                <p>
+                    Le développement des compétences par le biais de la formation professionnelle est une autre facette cruciale de notre intervention.
+                </p>
+                <p>
+                    Nous croyons fermement que l'apprentissage continu est essentiel pour s'adapter aux évolutions du marché du travail et aux nouvelles exigences professionnelles. La formation est l’un des leviers de la motivation. Nous concevons des programmes de formation personnalisés qui ciblent les lacunes spécifiques des individus et des équipes, en utilisant des méthodologies pédagogiques innovantes pour maximiser l'acquisition de connaissances et le développement des compétences.
+                </p>
             </div>
-            <div>
-                La gestion de la formation est une tâche complexe qui nécessite une approche rigoureuse et une expertise technique. Nous apportons notre savoir-faire en matière d'ingénierie de la formation et de gestion de la qualité pour optimiser les processus de formation et garantir des résultats efficaces. Nous veillons à ce que les programmes de formation soient bien structurés, basés sur des objectifs clairs, et évalués de manière objective afin de garantir leur pertinence et leur impact positif sur les participants.
+            <div className='AboutPres'>
+                <p>
+                    La gestion de la formation est une tâche complexe qui nécessite une approche rigoureuse et une expertise technique.
+                </p>
+                <p>
+                    Nous apportons notre savoir-faire en matière d'ingénierie de la formation et de gestion de la qualité pour optimiser les processus de formation et garantir des résultats efficaces. Nous veillons à ce que les programmes de formation soient bien structurés, basés sur des objectifs clairs, et évalués de manière objective afin de garantir leur pertinence et leur impact positif sur les participants.
+                </p>
             </div>
-            <div>
+            <div className='AboutPres-center'>
                 Nous sommes également conscients des risques psychosociaux qui peuvent affecter les entreprises, les salariés et les particuliers. Nous offrons des services de conseil et d'accompagnement pour identifier, prévenir et gérer ces risques, tels que le stress, le harcèlement ou l'épuisement professionnel. Notre approche holistique tient compte des facteurs organisationnels et individuels pour promouvoir un environnement de travail sain et équilibré.
             </div>
-            <div>
-                Notre objectif est d'apporter des solutions complètes et personnalisées à nos clients, en combinant notre expertise dans ces différents domaines pour favoriser un environnement de travail épanouissant, productif et équitable. Quels que soient les défis auxquels nos clients sont confrontés, nous sommes là pour les accompagner et les aider à atteindre leurs objectifs professionnels.
+            <div className='AboutPres'>
+                <p>
+                    Notre objectif est d'apporter des solutions complètes et personnalisées à nos clients...
+                </p>
+                <p>
+                    ... en combinant notre expertise dans ces différents domaines pour favoriser un environnement de travail épanouissant, productif et équitable. Quels que soient les défis auxquels nos clients sont confrontés, nous sommes là pour les accompagner et les aider à atteindre leurs objectifs professionnels.
+                </p>
             </div>
-            <div>
+            <div className='AboutPres-centeredText'>
                 AtTRIBUt représente une communauté de partenaires privés et publics et est en prise sur le marché du travail et ses mutations.
-                L’une de nos forces : La mise en réseau.
+                L’une de nos forces :
+                <p>
+                    La mise en réseau.
+                </p>
             </div>
-            <div>
+            <div className='AboutPres-center'>
                 Membre du Centre des Jeunes Dirigeants, nous contribuons à une démarche volontaire de performance globale
                 <ul>
                     <li>Respect du client</li>
@@ -52,8 +95,10 @@ const About = () => {
                     <li>Démarche RSE (membre fondatrice du groupe FACE)</li>
                 </ul>
             </div>
-            <div>
-                ATTRIBUT CONSEILS est titulaire du processus qualité́ <span>QUALIOPI</span>, certificat de qualification professionnelle, qui atteste de la qualité du processus mis en œuvre par les prestataires d’actions concourant au développement des compétences, qu’il s’agisse d’actions de formation, de bilans de compétences :
+            <div className='AboutPresQualiopi'>
+                <span>
+                    ATTRIBUT CONSEILS est titulaire du processus qualité́ <span>QUALIOPI</span>, certificat de qualification professionnelle, qui atteste de la qualité du processus mis en œuvre par les prestataires d’actions concourant au développement des compétences, qu’il s’agisse d’actions de formation, de bilans de compétences :
+                </span>
             </div>
             <ul>
                 <li>Le respect de la réglementation</li>
@@ -99,7 +144,7 @@ const About = () => {
                     <li>Le partage</li>
                 </ul>
             </div>
-        </div>
+        </section>
     );
 };
 
