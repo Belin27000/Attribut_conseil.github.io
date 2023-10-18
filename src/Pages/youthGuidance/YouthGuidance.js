894bd4649780b4d0ca20f5fd53574b87d48b82d5
Belin27000 <y.lecerf@protonmail.com>
2023-10-18 17:46:33 +0200

    build YouthGuidance page

--- src/Pages/youthGuidance/YouthGuidance.js
+++ src/Pages/youthGuidance/YouthGuidance.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './youthGuidance.scss'
+import ContactButton from '../../Components/Button/ContactButton.js';
 
 const YouthGuidance = () => {
     return (
         <section className='YouthGuidance'>
             <h2 className="YouthGuidance-title">ORIENTATION JEUNES</h2>
+            <h3 className="YouthGuidance-article-title">L’ORIENTATION: ÇA S’ÉDUQUE!</h3>
             <article className="YouthGuidance-article">
-                <h3 className="YouthGuidance-article-title">L’ORIENTATION: ÇA S’ÉDUQUE!</h3>
                 <p>
                     Quel parent ne se retrouve pas démunis face aux méandres de l'orientation scolaire et face à la difficulté qu'ont nos enfants à savoir répondre à ces grandes questions :
                 </p>
@@ -23,7 +24,74 @@ const YouthGuidance = () => {
                     <li>Vous doutez de vos capacités, de vos motivations ?</li>
                     <li>Vous voulez faire le point sur vos résultats scolaires ?</li>
                 </ul>
-
+                <p>Au catalogue des séances d'orientation, on retrouve trop souvent de simples procédures d'évaluation débouchant sur des formes de recommandations. Pour ATTRIBUT CONSEILS, il s'agit d'impliquer votre enfant ou le jeune adulte dans le processus de choix et d'en intégrer les mécanismes.</p>
+                <p>Il pourra intégrer dès aujourd'hui la démarche qui lui permettra d'être en capacité de prendre en main la gestion personnelle de son propre avenir et d'acquérir les habiletés qui lui permettront d'être autonome tout au long de sa vie professionnelle et personnelle. Car il sera sans cesse confronté à des périodes d'orientation, de réorientation, de changement de carrière. Vous le savez bien, car vous y êtes vous même de temps en temps confronté !</p>
+            </article>
+            <article className="YouthGuidance-article_bgWhite">
+                <h3 className="YouthGuidance-article_blue">
+                    Réserver votre bilan d’orientation dès maintenant, à distance ou en présentiel ou obtenez le programme :</h3>
+                <ContactButton text='Je contacte Attribut conseils' />
+            </article>
+            <article className="YouthGuidance-article">
+                <h3 className="YouthGuidance-article-title">LES ÉTAPES DE L’ACCOMPAGNEMENT</h3>
+                <h4 className="YouthGuidance-article-subtitle">Phase 1 : Soi</h4>
+                <ul>
+                    <li>A partir d'entretiens individuels, de tests et de questionnaires, nous dégageons vos motivations, vos centres d'intérêts. Nous explorons la dynamique de parcours et des réalisations scolaires et extra-scolaires, ferons un inventaire des compétences, un bilan de la personnalité et des potentiels.</li>
+                    <li>Nous évaluons vos résultats scolaires sur plusieurs années pour faire le point sur vos capacités et vos compétences.</li>
+                    <li>Nous analysons vos méthodes de travail personnel.</li>
+                    <li>Nous vous aidons à prendre conscience de votre potentiel.</li>
+                </ul>
+                <h4 className="YouthGuidance-article-subtitle">Phase 2 : Environnement</h4>
+                <ul>
+                    <li>Élaboration de pistes de projets scolaires et professionnels.</li>
+                    <li>Recherche d'informations et enquêtes terrain avec notre réseau de professionnelsafin de valider un ou des axes de développement (possibilité de faire une demi journée d'observation métier).</li>
+                    <li>Confrontation du projet avec la réalité économique et avec les perspectives d'évolution du marché.</li>
+                </ul>
+                <h4 className="YouthGuidance-article-subtitle">Phase 3 : Formalisation du projet – Choix</h4>
+                <ul>
+                    <li>Pistes, élaboration de projets scolaires et professionnels réalistes et réalisables</li>
+                    <li>Mise en oeuvre du plan d’action : principales étapes de la mise en œuvre du projet professionnel ou personnel</li>
+                    <li>Synthèse du bilan</li>
+                </ul>
+                <h4 className="YouthGuidance-article-title">Nos outils :</h4>
+                <ul>
+                    <li>Tests à partir de logiciels d'aide à l'orientation</li>
+                    <li>Questionnaires de profils d'intérêts</li>
+                    <li>Tests de motivations</li>
+                    <li>Base de données sur les métiers et les formations scolaires et professionnelles</li>
+                </ul>
+                <h4 className="YouthGuidance-article-title">Notre méthode : L’ORIENTATION ÉDUCATIVE</h4>
+                <p>Nous utilisons le concept de l'orientation éducative : <strong>L'Activation du Développement Vocationnel et personnel</strong> et les techniques de coaching pour permettre à votre jeune de réussir son orientation, car comment choisir en mettant toutes les chances de son coté ?</p>
+                <p><strong>L'activation du Développement Professionnel et Personnel : qu'est ce que c'est ??</strong></p>
+                <p>Allez, un peu de théorie !!</p>
+                <p>Ce modèle inspiré des recherches conduites aux Etats Unis, est né au Quebec. Pelletier, Noiseux, Ujold s'attachent à rendre compte de la dynamique complexe de choix professionnel et avancent la notion de développement vocationnel.</p>
+                <p>Il s'agit d'appliquer à la séquence vocationnelle un questionnement opératoire nécessitant la réussite de quatre grandes tâches, à savoir :</p>
+                <ol>
+                    <li>L'exploration :</li>
+                    <p>Envisager toutes les possibilités sans tenir compte de la réalité par un processus d'essais et d'erreurs.</p>
+                    <li>La cristallisation :</li>
+                    <p>Réduire par rapport à l'exploration, choisir un champ d'intérêt en terme de grandes lignes directrices.</p>
+                    <li>La spécification :</li>
+                    <p>Identifier les valeurs et les critères, et les sélectionner vers une option plus spécifique, évaluer le projet en fonction du désirable et du probable.</p>
+                    <li>La réalisation :</li>
+                    <p>Faire passer ses intentions au niveau du réel, poser les problèmes en termes concrets et pratiques, planifier, réviser les étapes.</p>
+                </ol>
+                <p>A chacune des étapes correspondent des tâches définies et ordonnées. La démarche d'orientation peut être comprise comme un cheminement qui permet à l'individu de se reconnaître tel qu'il est et tel qu'il se modifie.</p>
+            </article>
+            <article className="YouthGuidance-article_bgWhite">
+                <h3 className="YouthGuidance-article_blue">
+                    « Ici, l'expert n'est que l'expert de la méthode. Il a pour tâche principale de guider la personne à travers ses interrogations, ses désirs, ses découragements, ses demandes, sans se substituer à elle, mais tout simplement en l'accompagnant. »
+                </h3>
+            </article>
+            <article className="YouthGuidance-article">
+                <p>Ces séquences éducatives contribuent à <strong>accélérer le processus de cheminement en permettant au jeune de modifier notamment ses représentations initiales des professions.</strong> Elles agissent de ce fait contre les processus traditionnels de sélection qui conduisent à l'exclusion de nombreux jeunes.
+                </p>
+                <p>Ce faisant, elles permettent au jeune – <strong>de devenir acteur de son projet</strong> - de mieux utiliser les ressources de notre système de formation professionnelle ou initiale et de savoir naviguer ce système si complexe qu’est notre monde de l’emploi et de la formation.
+                </p>
+                <h3 className="YouthGuidance-article-title">Nos tarifs</h3>
+                <ul className='YouthGuidance-article-doc'>
+                    <li>Déontologie et éthique</li>
+                </ul>
             </article>
         </section>
     );
