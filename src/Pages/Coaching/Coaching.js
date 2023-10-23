aafffef8bc45ca41b559549dc9aa6e2785632176
Belin27000 <y.lecerf@protonmail.com>
2023-10-23 10:36:03 +0200

    feat: first paragraphe on  building coaching page

--- src/Pages/Coaching/Coaching.js
+++ src/Pages/Coaching/Coaching.js
@@ -10,6 +10,24 @@ const Coaching = () => {
                 </p>
                 <p>L'accompagnement en coaching professionnel s'adresse tant aux <strong>particuliers qu'aux entreprises</strong>. Vous bénéficierez d'un soutien assuré par un coach certifié et diplômé en intervention systémique et stratégique .
                 </p>
+                <h3 className="Coaching-article-title">LES RESULTATS ATTENDUS DU COACHING
+                </h3>
+                <ul>
+                    <li>Le client atteint un niveau élevé de performance (par rapport à une norme objective) dans le long terme
+                    </li>
+                    <li>Le client s’améliore constamment, il est dans une dynamique de changement. Les changements sont génératifs: ils en appellent d’autres.
+                    </li>
+                    <li>Le client résout une problématique professionnelle
+                    </li>
+                    <li>Le client oriente ses actions vers l'atteinte des résultats attendus
+                    </li>
+                </ul>
+                <p className='Coaching-article_center'><strong>Performance excellente dans le long terme
+                </strong></p>
+                <p className='Coaching-article_center'><strong>Auto - correction
+                </strong></p>
+                <p><strong className='Coaching-article_center'>Auto - génération
+                </strong></p>
             </article>
         </section>
     );
