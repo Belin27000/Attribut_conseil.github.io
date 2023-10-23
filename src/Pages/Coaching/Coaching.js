93558948e86fede7f5797ed7f301e560b653410b
Belin27000 <y.lecerf@protonmail.com>
2023-10-23 10:28:09 +0200

    feat: start building coaching page

--- src/Pages/Coaching/Coaching.js
+++ src/Pages/Coaching/Coaching.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './coaching.scss'
 
 const Coaching = () => {
     return (
         <section className='Coaching'>
-            test
+            <h2 className="Coaching-title">COACHING</h2>
+            <article className="Coaching-article">
+                <p>Notre service d'accompagnement en coaching axé sur la systémie est conçu pour vous aider à comprendre les relations et les dynamiques qui vous entourent, et à les utiliser de manière constructive pour atteindre vos objectifs.
+                </p>
+                <p>L'accompagnement en coaching professionnel s'adresse tant aux <strong>particuliers qu'aux entreprises</strong>. Vous bénéficierez d'un soutien assuré par un coach certifié et diplômé en intervention systémique et stratégique .
+                </p>
+            </article>
         </section>
     );
 };
