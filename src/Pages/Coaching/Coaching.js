c7795c51f40e4ad8d2c4b0ac5da79c25de8c3445
Belin27000 <y.lecerf@protonmail.com>
2023-11-01 11:17:57 +0100

    feat: Coaching page done

--- src/Pages/Coaching/Coaching.js
+++ src/Pages/Coaching/Coaching.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './coaching.scss'
+import ContactButton from '../../Components/Button/ContactButton.js';
 
 const Coaching = () => {
     return (
@@ -22,13 +23,49 @@ const Coaching = () => {
                     <li>Le client oriente ses actions vers l'atteinte des résultats attendus
                     </li>
                 </ul>
-                <p className='Coaching-article_center'><strong>Performance excellente dans le long terme
+                <p className='Coaching-article center'><strong>Performance excellente dans le long terme
                 </strong></p>
-                <p className='Coaching-article_center'><strong>Auto - correction
+                <p className='Coaching-article center'><strong>Auto - correction
                 </strong></p>
-                <p><strong className='Coaching-article_center'>Auto - génération
+                <p className='Coaching-article center'><strong>Auto - génération
                 </strong></p>
+                <h3 className="Coaching-article-title">APPROCHE DE NOTRE DÉMARCHE SYSTÉMIQUE</h3>
+                <p>L’approche proposée par ATTRIBUT CONSEILS est à la fois systémique, paradoxale, stratégique et non normative :</p>
+                <p><strong>Systémique</strong> au sens où elle pose un regard interactionnel sur la situation amenée par le client, et permet un décodage spécifique des interactions et des difficultés qui se jouent entre le client et son environnement ou entre le client et lui-même ; elle est systémique également car elle permet de sélectionner les éléments pertinents au sein de l’environnement dans lequel le client évolue.</p>
+                <p><strong>Paradoxale</strong>, car elle se donne comme axe d’intervention principal de repérer et contrer les tentatives de régulation du client : c’est-à-dire ce qu’il met en place comme solutions mais qui est inopérant ou contribue au maintien de la problématique… à l’origine de sa demande de changement !</p>
+                <p><strong>Stratégique</strong> parce qu’elle confère à l’intervenant une posture de co-construction, avec le client,de régulations nouvelles qui lui permettent d’expérimenter et de s’approprier des modalités de changement effectives ;avec une influence assumée de l’intervenant notamment par la prescription de tâches (ou d’exercices) contribuant à un mouvement d’inversion et d’ouverture permettant au client de vivre des nouvelles expériences qui s’avèrent «correctrices».</p>
+                <p><strong>Non normative</strong> car elle respecte entièrement et intègre la vision du monde et la position du client ; et aussi car elles ne s’appuie pas sur des règles ou modes d’emploi pour induire le changement : elle s’appuie sur un décodage précis et contextualisé de l’évaluation du caractère fonctionnel ou non de ce que la personne met en place pour résoudre sa problématique et atteindre ses objectifs de changement.</p>
+                <h3 className="Coaching-article-title">PHASE PRÉLIMINAIRE AU COACHING</h3>
+                <p><strong>Un entretien préalable gratuit et sans engagement </strong> est organisé avec le client afin d’établir le contexte/la situation proposé pour travailler ensemble de façon à être sur que l’on parle de la même chose et qu’il n’y a pas de dérive vers un autre champ de travail (à moins que cela ne soit décidé). Les règles de confort et de sécurité dont le bénéficiaire a besoin seront exprimées. L’objectif privé et/ou public qu’il souhaite atteindre et les indicateurs de résultats.</p>
+                <p><strong>La confidentialité</strong> étant la règle principale de la déontologie, nous verrons ensemble ce dont le client souhaite ou pas aborder en réunion tripartite, si il y a lieu, avec son entreprise.
+                </p>
+                <p><strong>Dans le cas d’une commande de l’entreprise, </strong>, une réunion tripartite d’1h avec le responsable hiérarchique et le bénéficiaire sera organisée. L’objectif public sera fixé. Un compromis devra être réalisé entre l’objectif privé (celui défini en entretien préalable) et l’objectif dit public, c’est à dire celui du responsable hiérarchique. Le coaching ne pouvant fonctionner sous la contrainte.</p>
+                <p>Les indicateurs de résultats, lieu, dates, fréquence, condition d’annulation seront contractualisés.</p>
             </article>
+            <article className="Coaching-article_bgWhite">
+                <h3 className="Bilan-article_blue">Réserver votre coaching dès maintenant, à distance ou en présentiel : </h3>
+                <ContactButton />
+            </article>
+            <article className="Coaching-article">
+                <h3 className="Coaching-article-title">DEROULEMENT DES SÉANCES</h3>
+                <p><strong>La première phase </strong>consiste à mettre au travail la demande de coaching, c’est à dire, en faciliter l’expression et en permettre l’élaboration dans ses dimensions historiques, contextuelles, et dans ses enjeux. Cette étape va aboutir à la formulation précise de l’objectif. Construit par la personne avec le concours du coach, il correspond à la cristallisation d’un aspect de la demande ou d’un ensemble d’éléments qui la compose. Sa formulation aura toute son importance car l’objectif sera exprimé avec les mots même de la personne.</p>
+                <p><strong>La deuxième phase</strong>se focalise sur les interactions pour déclencher et générer les changements attendus. Les interactions, que l’on peut appeler « relations d’un individu » sont multiples autant qu’inépuisables. Les relations sont celles entretenues avec les autres, mais pas seulement. Il s’agit de travailler la relation que la personne entretient :</p>
+                <ul>
+                    <li>Avec elle même</li>
+                    <li>Avec son objectif de performance</li>
+                    <li>Avec le problème</li>
+                    <li>Avec son environnement de travail</li>
+                </ul>
+                <p>Dans cette perspective, l’accent n’est pas mis sur la personne elle-même, isolée de son contexte. De fait, le questionnement du coach sera orienté vers les interactions, leurs descriptions notamment pour en dégager les éléments utiles.</p>
+                <p><strong>La troisième phase </strong>concerne le changement de perspective. Penser le problème en termes de solution et non plus en termes de problème. Des exercices d’externalisation seront alors mis en place. Le client élabore un champ d’options qu’il mettra en action en dehors des séances de coaching. Le changement de perspective du point de vue de la pratique consiste alors à ne pas chercher à résoudre des problèmes mais à construire de nouvelles façons d‘agir et d’interagir. Cela va permettre à la personne de mobiliser ses points forts, ses capacités et ses qualités. Cela repose sur un travail spécifique pour décrire et recenser l’ensemble des compétences (pris au sens compétences relationnelles, personnelles) sur lesquelles s’appuyer. Cela vise aussi à identifier dans la situation professionnelle, ce que la personne fait, sait faire, et qui marche comme autant d’actions qui seront utiles pour atteindre l’objectif.</p>
+                <p>Cette phase consiste à élaborer de nouvelles formes de représentations, de nouvelles façons de construire la réalité.</p>
+                <h3 className="Coaching-article-title">Nous nous focalisons sur ce qui est le plus important pour vous !</h3>
+                <p>En tant qu'êtres humains, nous sommes constamment influencés par les systèmes dans lesquels nous évoluons : famille, travail, relations sociales, etc. Ces systèmes interconnectés ont un impact significatif sur notre vie et notre bien-être. Notre approche de coaching systémique vous donne les outils et les compétences nécessaires pour naviguer efficacement au sein de ces systèmes et créer des changements positifs.</p>
+                <p>Libérez votre plein potentiel en comprenant et en naviguant habilement à travers les systèmes complexes qui vous entourent.</p>
+                <h3 className="Coaching-article-title">Nos tarifs</h3>
+                <p>Le coût du coaching est évalué lors d'un entretien préliminaire et varie en fonction de la situation du client ainsi que des modalités de financement.</p>
+            </article>
+
         </section>
     );
 };
